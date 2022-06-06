54cf5bbf06d0a1400c03795b54be21847cf287d7
TheVoidZero <aldohernandezcruz115@gmail.com>
2022-06-06 01:59:17 -0500

    asistente virtual usuario agrega cosas vercion 2.8

--- liz_gui.py
+++ liz_gui.py
@@ -38,8 +38,12 @@ canvas_comandos=Canvas(bg="#654ea3", height=200,width=195)
 canvas_comandos.place(x=0,y=0)
 canvas_comandos.create_text(90,80,text=comandos,fill="white",font='Arial 10')
 
+
+text_info = Text(main_window,bg="#654ea3",fg="white")
+text_info.place(x=0,y=200,height=200,width=195)
+
 text_info = Text(main_window, bg="#654ea3", fg="white")
-text_info.place(x=0, y=200,height=430,width=200)
+text_info.place(x=0, y=200,height=400,width=200)
 
 
 
@@ -70,25 +74,12 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 engine.setProperty('rate', 145)
 
-sites = {
-    'google': 'https://www.google.com.mx/',
-    'youtube': 'https://www.youtube.com/',
-    'facebook': 'https://www.facebook.com/',
-    'whatsapp': 'https://web.whatsapp.com/',
-    'tu manga': 'https://lectortmo.com/',
-}
 
-files = {
-    'carta': 'Carta pasantia-Aldo Hernadnez.pdf',
-    'cedula': 'Cedula y papeleta-aldo hernandez.docx',
-    'foto': 'aldo.jpg'
+sites = dict()
 
-}
+files = dict()
 
-programas = {
-    'steam': r'D:\Steam\steam.exe'  # la r nos indica que lo ocuparemos como un solo string por el \
-
-}
+programas = dict()
 
 
 def talk(text):
@@ -196,6 +187,89 @@ def write(f):
     talk("Listo, puedes revisarlo")
     sub.Popen("nota.txt", shell=True)
 
+def open_files_w():
+    window_files=Toplevel()
+    window_files.title("Agregar archivos")
+    window_files.configure(bg= "#654ea3")
+    window_files.geometry("300x200")
+    window_files.resizable(0,0)
+    main_window.eval(f'tk::PlaceWindow {str(window_files)} center')
+
+    title_label= Label(window_files, text="Agrega un archivo", bg="#654ea3", fg="white",font=('Arial',"15", 'bold'))
+    title_label.pack(pady=3)
+
+    name_label= Label(window_files, text="Nombre del archivo", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    name_label.pack(pady=2)
+
+    namefile_entry= Entry(window_files)
+    namefile_entry.pack(pady=1)
+
+    path_label= Label(window_files, text="Ruta del archivo", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    path_label.pack(pady=2)
+
+    path_entry= Entry(window_files,width=35)
+    path_entry.pack(pady=1)
+
+    save_button= Button(window_files, text="Guardar", bg="#654ea3", fg="white",width=8,height=1)
+    save_button.pack(pady=4)
+
+
+
+def open_apps_w():
+    window_apps=Toplevel()
+    window_apps.title("Agregar apps")
+    window_apps.configure(bg= "#654ea3")
+    window_apps.geometry("300x200")
+    window_apps.resizable(0,0)
+    main_window.eval(f'tk::PlaceWindow {str(window_apps)} center')
+
+    title_label= Label(window_apps, text="Agrega app", bg="#654ea3", fg="white",font=('Arial',"15", 'bold'))
+    title_label.pack(pady=3)
+
+    name_label= Label(window_apps, text="Nombre de la app", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    name_label.pack(pady=2)
+
+    nameapps_entry= Entry(window_apps)
+    nameapps_entry.pack(pady=1)
+
+    path_label= Label(window_apps, text="Ruta de la app", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    path_label.pack(pady=2)
+
+    path_entry= Entry(window_apps,width=35)
+    path_entry.pack(pady=1)
+
+    save_button= Button(window_apps, text="Guardar", bg="#654ea3", fg="white",width=8,height=1)
+    save_button.pack(pady=4)
+
+
+def open_pages_w():
+    window_pages=Toplevel()
+    window_pages.title("Agregar paginas")
+    window_pages.configure(bg= "#654ea3")
+    window_pages.geometry("300x200")
+    window_pages.resizable(0,0)
+    main_window.eval(f'tk::PlaceWindow {str(window_pages)} center')
+
+    title_label= Label(window_pages, text="Agrega una pagina", bg="#654ea3", fg="white",font=('Arial',"15", 'bold'))
+    title_label.pack(pady=3)
+
+    name_label= Label(window_pages, text="Nombre de la pagina", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    name_label.pack(pady=2)
+
+    namepages_entry= Entry(window_pages)
+    namepages_entry.pack(pady=1)
+
+    path_label= Label(window_pages, text="Ruta de la pagina", bg="#654ea3", fg="white",font=('Arial',"10", 'bold'))
+    path_label.pack(pady=2)
+
+    path_entry= Entry(window_pages,width=35)
+    path_entry.pack(pady=1)
+
+    save_button= Button(window_pages, text="Guardar", bg="#654ea3", fg="white",width=8,height=1)
+    save_button.pack(pady=4)
+
+
+
 button_voice_mx=Button(main_window, text="Voz mexicana", fg="white", bg="#348F50", font=("Arial",10,"bold"),command=mexican_voice)
 button_voice_mx.place(x=625,y=90,width=100,height=30)
 button_voice_es=Button(main_window, text="Voz española", fg="white", bg="#b92b27", font=("Arial",10,"bold"),command=spanish_voice)
@@ -206,6 +280,12 @@ button_listen=Button(main_window, text="Escuchar", fg="white", bg="#c31432", fon
 button_listen.pack(pady=10)
 button_speak=Button(main_window, text="Hablar", fg="white", bg="#654ea3", font=("Arial",10,"bold"),width=10,height=4,command=read_and_talk)
 button_speak.place(x=625,y=210,width=100,height=30)
+button_add_files=Button(main_window, text="Agregar archivos", fg="white", bg="#654ea3", font=("Arial",10,"bold"),width=10,height=4,command=open_files_w)
+button_add_files.place(x=615,y=250,width=120,height=30)
+button_add_apps=Button(main_window, text="Agregar aplicaciones", fg="white", bg="#654ea3", font=("Arial",10,"bold"),width=10,height=4,command=open_apps_w)
+button_add_apps.place(x=615,y=290,width=150,height=30)
+button_add_pages=Button(main_window, text="Agregar paginas", fg="white", bg="#654ea3", font=("Arial",10,"bold"),width=10,height=4,command=open_pages_w)
+button_add_pages.place(x=615,y=330,width=120,height=30)
 
 
 
